f644acc425f6000f00c0c1ecc411773e855c1217
Tan Tai <trantantai310803@gmail.com>
2025-05-21 21:31:40 +0700

    create

---
+++ src/main/java/org/example/ktigerstudybe/KtigerstudybeApplication.java
@@ -0,0 +1,13 @@
+package org.example.ktigerstudybe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtigerstudybeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KtigerstudybeApplication.class, args);
+    }
+
+}
